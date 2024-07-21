d0c65df4d4531b6b2c59115c2bef689bb63dbc64
offline <a.bensalah.net@gmail.com>
2024-07-21 16:33:12 +0200

    2

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,28 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.querySelector('.menu');
     const menuIcon = document.querySelector('.menu-icon');
-  
-    const toggleMenu = () => {
-      menu.classList.toggle('active');
-      menuIcon.classList.toggle('active');
-    };
+    const overlayMenu = document.querySelector('.overlay-menu');
   
     menuIcon.addEventListener('click', () => {
-      toggleMenu();
+      menuIcon.classList.toggle('active');
+      overlayMenu.classList.toggle('active');
     });
   
     window.addEventListener('resize', () => {
       if (window.innerWidth > 768) {
-        menu.classList.add('active');
         menuIcon.classList.remove('active');
-      } else {
-        menu.classList.remove('active');
-        menuToggle.checked = false;
+        overlayMenu.classList.remove('active');
       }
     });
-  
-    if (window.innerWidth > 768) {
-      menu.classList.add('active');
-    }
   });
